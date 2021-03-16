4e0a80fd148dd9a4f37c96b43ef6b4339b6ca6a0
mazhihui <mazhihui@liepin.com>
2021-03-16 16:03:39 +0800

    modify some files

--- init.sh
+++ init.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -xeu -o pipefail
 
-yum install -y -q yum-utils iptables git vim
+echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
+sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+setenforce 0
+sed -i 's/authtok_type= enforce_for_root//' /etc/pam.d/system-auth
+
+yum erase -y buildah podman
+yum install -y -q yum-utils iptables git vim epel-release
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y -q docker-ce
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 systemctl enable --now docker
-
-mkdir -p ~/{geth,nucypher}
-
-cat >> ~/.bashrc << EOF
-alias geth-console='docker-compose exec geth geth attach /root/.ethereum/geth.ipc'
-alias nucypher-init='docker-compose run --rm nucypher nucypher ursula init --provider ipc:///root/.ethereum/geth.ipc'
-EOF
 
 git clone https://github.com/paco0x/nu-scripts
